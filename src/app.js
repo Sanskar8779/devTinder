6e1dc47f98b2004338aa375ae22dfd399adfd383
Sanskar <sanskar.shubham28@gmail.com>
2025-01-01 22:34:55 +0530

    feed api

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ app.use(express.json()); //this will convert json into JS object
 
 app.post("/signup", async (req, res) => {
 	//creating an instance of USer model
-
 	const user = new User(req.body);
 
 	try {
@@ -16,6 +15,36 @@ app.post("/signup", async (req, res) => {
 		res.send("User added successfully!");
 	} catch (err) {
 		res.status(400).send("Error saving the user", err);
+	}
+});
+
+//Get one user by email
+app.get("/user", async (req, res) => {
+	const userEmail = req.body.email;
+
+	try {
+		const user = await User.findOne({ email: userEmail });
+		if (!user) {
+			res.status(404).send("User not found");
+		} else {
+			res.send(user);
+		}
+	} catch (err) {
+		res.status(400).send("Something went wrong!!!");
+	}
+});
+
+//Get all users
+app.get("/feed", async (req, res) => {
+	try {
+		const users = await User.find({});
+		if (users.length === 0) {
+			res.status(404).send("Users not found");
+		} else {
+			res.send(users);
+		}
+	} catch (err) {
+		res.status(400).send("Something went wrong!!!");
 	}
 });
 
